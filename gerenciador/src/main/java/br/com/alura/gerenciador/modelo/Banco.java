731e52670444ce3419bfa9affa31921a953e232e
CleudonCarvalho <85129269+CleudonCarvalho@users.noreply.github.com>
2021-07-27 13:16:26 -0300

    commit crud exemplo gerenciador  alura

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/modelo/Banco.java
@@ -0,0 +1,74 @@
+package br.com.alura.gerenciador.modelo;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Banco {
+	
+	private static List<Empresa> lista = new ArrayList<>();
+	private static List<Usuarioo> listaUsuarios = new ArrayList<>();
+	private static Integer chaveSequencial = 1;
+	
+	static {
+		Empresa empresa = new Empresa();
+		empresa.setId(chaveSequencial++);
+		empresa.setNome("Alura");
+		Empresa empresa2 = new Empresa();
+		empresa2.setId(chaveSequencial++);
+		empresa2.setNome("Caelum");
+		lista.add(empresa);
+		lista.add(empresa2);
+		
+		Usuarioo u1 = new Usuarioo();
+		u1.setLogin("cludu");
+		u1.setSenha("123senhanovaop");
+		Usuarioo u2 = new Usuarioo();
+		u2.setLogin("Thaize");
+		u2.setSenha("131502Boo");
+		
+		listaUsuarios.add(u1);
+		listaUsuarios.add(u2);
+	}
+
+	public void adiciona(Empresa empresa) {
+		empresa.setId(Banco.chaveSequencial++);
+		Banco.lista.add(empresa);
+	}
+	
+	public List<Empresa> getEmpresas(){
+		return Banco.lista;
+	}
+
+	public void removeEmpresa(Integer id) {
+		
+		Iterator<Empresa> it = lista.iterator();
+		
+		while(it.hasNext()) {
+			Empresa emp = it.next();
+			
+			if(emp.getId() == id) {
+				it.remove();
+			}
+		}
+	}
+
+	public Empresa buscaEmpresaPelaId(Integer id) {
+		for (Empresa empresa : lista) {
+			if(empresa.getId() == id) {
+				return empresa;
+			}
+		}
+		return null;
+	}
+
+	public Usuarioo existeUsuario(String login, String senha) {
+		for(Usuarioo usuario : listaUsuarios) {
+			if(usuario.ehIgual(login, senha)) {
+				return usuario;
+			}
+		}
+		return null;
+	}
+
+}
